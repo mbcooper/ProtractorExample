abe16f476ff6304ae40e8e20a847d91b784c5d4d
mbcooper <mike@cooperusa.net>
2015-02-05 10:35:39 -0500

    initial committ

---
+++ e2e/Home.po.js
@@ -0,0 +1,75 @@
+/**
+ * Created by mike on 2/5/2015.
+ */
+var _ = require('lodash');
+var config = require('./conf.js').config;
+
+var env = config.env;
+
+'use strict';
+/**
+ * @class HomePage
+ * @constructor
+ */
+var HomePage = function() {
+
+  browser.get(env.baseUrl + '/home');
+};
+
+HomePage.prototype = Object.create(
+  {}, {
+    viewTitle: {
+      get: function() {
+
+        return browser.getTitle();
+      }
+    },
+    searchBox: {
+      get: function() {
+        return element(by.model('home.search'))
+      }
+    },
+
+    searchButton: {
+      get: function() {
+        return element(by.id('searchButton'));
+      }
+    },
+
+    enterSearch: {
+      value: function(keys) {
+        return this.searchBox.sendKeys(keys);
+      }
+    },
+    clearSearch: {
+      value: function() {
+
+        return this.searchBox.clear();
+      }
+    },
+    clickSearch: {
+      value: function() {
+        return this.searchButton.click();
+      }
+    },
+
+    allResults: {
+      get: function() {
+        return element.all(by.model('home.results'));
+      }
+    },
+
+    badTextMessage: {
+      get: function() {
+        return element(by.css('[ng-message="pattern"]')).getText();
+      }
+    },
+    longMessage: {
+      get: function() {
+        return element(by.css('[ng-message="maxlength"]')).getText();
+      }
+    }
+  }
+);
+
+module.exports = LoginPage;
---
+++ readMe.md
@@ -0,0 +1,29 @@
+#Sample Protractor Project
+
+This project is an illustration of the use of [Angular Protractor](http://angular.github.io/protractor/#/) on a simple Angular project.  It illustrates the use of [Page Objects](http://angular.github.io/protractor/#/page-objects) to abstract the DOM markup from tests, and to encourage re-use.  You'll see the tests are very fluent.
+
+## Setup
+
+1. Clone thus repo.
+1. Install node.exe if you haven't already. See [here](http://nodejs.org/).
+1. If you don't have gulp installed globally, do so with 
+	```javascript
+	npm install -g gulp-cli
+	```
+1. In the directory where you placed the project, set up the node modules.
+	```javascript
+	npm install
+	```
+1. You need to update the protractor and the selenium drivers
+	```javascript
+	npm install -g protractor
+	webdriver-manager update
+	```
+	
+Whew!  this gets you a project that will run off a simple home pages that allows a search in a bio database.  
+To simplify things, I serve up the results locally, and only **food** and **trees** are valid search terms.  The point of this sample is show how to test.
+
+##The App
+The app is a simple screen that presents a search box and a button.  Results are displayed (if any) below the search
+
+###Search
